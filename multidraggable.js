a6bf7367ac75f0c8a1276eba8e4e25bc93998885
Vinícius Muniz <vmunizm@gmail.com>
2016-12-13 14:27:03 -0300

    Update multidraggable.js

--- multidraggable.js
+++ multidraggable.js
@@ -7,43 +7,43 @@
  *
  * MultiDraggable is a jQuery plugin which extends jQuery UI Draggable to add multi drag and live functionality.
  *
-**/
+ **/
 (function ($, undefined) {
-   $.fn.multiDraggable = function (opts) {
-   	var initLeftOffset = []
-   	    ,initTopOffset = [];	    
-   	return this.each (function (){
-   		 $(this).live("mouseover", function() {
-		         if (!$(this).data("init")) {
-		            $(this).data("init", true).draggable(opts,{
-				         start: function (event,ui) {
-							var pos = $(this).position();
-							$.each(opts.group || {}, function(key,value) {
-								var elemPos = $(value).position();
-								initLeftOffset[key] = elemPos.left - pos.left;
-								initTopOffset[key] = elemPos.top - pos.top;			
-							});		
-							opts.startNative ? opts.startNative() : {};
-						},
-						drag: function(event,ui) {
-							var pos = $(this).offset();
-							$.each(opts.group || {}, function(key,value) {
-								$(value).offset({left: pos.left + initLeftOffset[key], 
-								top: pos.top + initTopOffset[key]});				
-							});
-							opts.dragNative ? opts.dragNative() : {};
-						},
-						stop: function(event,ui) {
-							var pos = $(this).offset();
-							$.each(opts.group || {}, function(key,value) {
-								$(value).offset({left: pos.left + initLeftOffset[key], 
-								top: pos.top + initTopOffset[key]});								
-							});
-							opts.stopNative ? opts.stopNative() : {};
-						},
-					});
-		        }
-	      });
- 	 });
-   };
- }(jQuery));
+    $.fn.multiDraggable = function (opts) {
+        var initLeftOffset = []
+            ,initTopOffset = [];
+        return this.each (function (){
+            $(this).on("mouseover", function() {
+                if (!$(this).data("init")) {
+                    $(this).data("init", true).draggable(opts,{
+                        start: function (event,ui) {
+                            var pos = $(this).position();
+                            $(opts.group).each( function(key,value) {
+                                var elemPos = $(value).position();
+                                initLeftOffset[key] = elemPos.left - pos.left;
+                                initTopOffset[key] = elemPos.top - pos.top;
+                            });
+                            opts.startNative ? opts.startNative(event,ui) : {};
+                        },
+                        drag: function(event,ui) {
+                            var pos = $(this).offset();
+                            $(opts.group).each( function(key,value) {
+                                $(value).offset({left: pos.left + initLeftOffset[key],
+                                    top: pos.top + initTopOffset[key]});
+                            });
+                            opts.dragNative ? opts.dragNative(event,ui) : {};
+                        },
+                        stop: function(event,ui) {
+                            var pos = $(this).offset();
+                            $(opts.group).each( function(key,value) {
+                                $(value).offset({left: pos.left + initLeftOffset[key],
+                                    top: pos.top + initTopOffset[key]});
+                            });
+                            opts.stopNative ? opts.stopNative(event,ui) : {};
+                        },
+                    });
+                }
+            });
+        });
+    };
+}(jQuery));
